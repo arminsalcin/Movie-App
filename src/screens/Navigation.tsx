89efaf4859252803f87c64b0d0a1a13ecb8d06c6
arminsalcin1 <armin.salcin1@gmail.com>
2020-08-24 17:05:01 +0200

    exclude some imports

--- src/screens/Navigation.tsx
+++ src/screens/Navigation.tsx
@@ -1,5 +1,5 @@
-import React, { JSXElementConstructor } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Details from './Details';
